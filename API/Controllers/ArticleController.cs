49a707b14362d87eff4d0d4d3faf533cfa7fa050
İbrahim Bali <balii@pm.me>
2020-09-23 22:52:43 +0300

    feat: add controller interface

---
+++ API/Controllers/ArticleController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleService _service;
+
+        public ArticleController(IArticleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var rows = await _service.GetAsync(id);
+            return Ok(rows);
+        }
+
+        [HttpPost("search")]
+        public async Task<IActionResult> SearchAsync(Article model)
+        {
+            var rows = await _service.SearchAsync(model);
+            return Ok(rows);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAsync(Article model)
+        {
+            var rows = await _service.AddAsync(model);
+            return Ok(rows);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync(Article model)
+        {
+            var rows = await _service.UpdateAsync(model);
+            return Ok(rows);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var rows = await _service.DeleteAsync(id);
+            return Ok(rows);
+        }
+    }
+}
---
+++ Service/IArticleService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Model;
+
+namespace Service
+{
+    public interface IArticleService
+    {
+        Task<IEnumerable<IArticle>> GetAsync(int id);
+        Task<IEnumerable<IArticle>> SearchAsync(IArticle model);
+        Task<bool> AddAsync(IArticle model);
+        Task<bool> UpdateAsync(IArticle model);
+        Task<bool> DeleteAsync(int id);
+    }
+}
